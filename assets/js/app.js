ec9c006b4d7a65f0e8c1b6f63d2ff26a00759930
Roger <rdpender3@gmail.com>
2019-11-17 13:18:21 -0800

    most elements on page and timer working

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,45 +1,46 @@
-// window.onload = function(e) {
-//   var $clock = $("#clock"),
-//     eventTime = moment("14-06-2020 13:00:00", "DD-MM-YYYY HH:mm:ss"),
-//     currentTime = moment(),
-//     diffTime = eventTime - currentTime,
-//     duration = moment.duration(diffTime * 1000, "milliseconds"),
-//     interval = 1000;
+$(document).ready(function() {
+  function countdown(endDate) {
+    let days, hours, minutes, seconds;
 
-//   this.console.log(currentTime, "current");
-//   this.console.log(eventTime, "event");
-//   this.console.log(duration, "dif");
+    endDate = new Date(endDate).getTime();
 
-//   // if time to countdown
-//   if (diffTime > 0) {
-//     // Show clock
-//     // $clock.show();
+    if (isNaN(endDate)) {
+      return;
+    }
 
-//     var $d = $('<div class="days" ></div>').appendTo($clock),
-//       $h = $('<div class="hours" ></div>').appendTo($clock),
-//       $m = $('<div class="minutes" ></div>').appendTo($clock),
-//       $s = $('<div class="seconds" ></div>').appendTo($clock);
+    setInterval(calculate, 1000);
 
-//     setInterval(function() {
-//       duration = moment.duration(
-//         duration.asMilliseconds() - interval,
-//         "milliseconds"
-//       );
-//       var d = moment.duration(duration).days(),
-//         h = moment.duration(duration).hours(),
-//         m = moment.duration(duration).minutes(),
-//         s = moment.duration(duration).seconds();
+    function calculate() {
+      let startDate = new Date().getTime();
 
-//       d = $.trim(d).length === 1 ? "0" + d : d;
-//       h = $.trim(h).length === 1 ? "0" + h : h;
-//       m = $.trim(m).length === 1 ? "0" + m : m;
-//       s = $.trim(s).length === 1 ? "0" + s : s;
+      let timeRemaining = parseInt((endDate - startDate) / 1000);
 
-//       // show how many hours, minutes and seconds are left
-//       $d.text(d);
-//       $h.text(h);
-//       $m.text(m);
-//       $s.text(s);
-//     }, interval);
-//   }
-// };
+      if (timeRemaining >= 0) {
+        days = parseInt(timeRemaining / 86400);
+        timeRemaining = timeRemaining % 86400;
+
+        hours = parseInt(timeRemaining / 3600);
+        timeRemaining = timeRemaining % 3600;
+
+        minutes = parseInt(timeRemaining / 60);
+        timeRemaining = timeRemaining % 60;
+
+        seconds = parseInt(timeRemaining);
+
+        document.getElementById("days").innerHTML = parseInt(days, 10);
+        document.getElementById("hours").innerHTML =
+          hours < 10 ? "0" + hours : hours;
+        document.getElementById("minutes").innerHTML =
+          minutes < 10 ? "0" + minutes : minutes;
+        document.getElementById("seconds").innerHTML =
+          seconds < 10 ? "0" + seconds : seconds;
+      } else {
+        return;
+      }
+    }
+  }
+
+  (function() {
+    countdown("06/14/2020 01:00:00 PM");
+  })();
+});
